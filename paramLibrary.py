707391b8c69ac34239c8c9b0d938e16f70756845
Alex Caldwell <alex@dynamic-oit-vapornet100-10-9-90-168.princeton.edu>
2021-12-09 16:57:00 -0500

    added constants for anode/cathodes of different lithium-ion cells

--- paramLibrary.py
+++ paramLibrary.py
@@ -1,32 +1,44 @@
 """
-Information for battery parameters.
-Each anode and cathode have specific string names.
-String names are attached to constants.
+Information for battery anode and cathode parameters.
 
+There are 3 types of cathodes: LFP, LCO, and NMC.
+Each cathode has parameters for diffusivity,
+particle radius, and maximum ion concentration. 
+
+Similarly, there are 2 types of anodes: 
+
+diffusivity,particle_radius,max_ion_concentration
 """
 import pprint
 
-def dictTest():
-    thisdict = {
-    "LFP": [],
-    "NMC": [],
-    "LCO": [],
-    "NCA": [],
+def cathodeDict():
+    cathdict = {
+    "LFP": [5.900E-18, 1.000E-8, 2.281E4],
+    "LCO": [1.736E-14, 1.637E-7, 1.035E4],
+    "NMC": [1.000E-14, 5.300E-6, 4.823E4],
+    # "NCA": [],
     }
-    return thisdict
+    return cathdict
 
-class immutableDict:
+class immutableCathDict:
     def __init__(self):
-        self.params = dictTest()
+        self.params = cathodeDict()
         pass
     def getParams(self):
         return self.params.copy()
-    def addParams(self, name, dif, rad, conc):
-        if name in self.params:
-            return
-        self.params[name] = [dif, rad, conc]
-        pass
 
-        
+def anodeDict():
+    anodedict = {
+    "GRAPHITE": [3.300E-14, 5.860E-6, 3.313E4],
+    }
+    return anodedict
+
+class immutableAnodeDict:
+    def __init__(self):
+        self.params = anodeDict()
+        pass
+    def getParams(self):
+        return self.params.copy()
+
 if __name__ == "__main__":
-    dictTest()
+    cathodeDict()
