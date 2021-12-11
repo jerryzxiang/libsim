192960c13e635295875bd73034d35bd629baaf05
Antti Valkonen <valkonen@princeton.edu>
2021-12-11 17:07:45 -0500

    reorganized the project

--- derivative.py
+++ derivative.py
@@ -1,21 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 22 14:56:26 2021
+Created on Sat Dec 11 17:03:26 2021
 
 @author: antti
 """
 
-from abc import ABC, abstractmethod
+
+import numpy as np
 
 
-class Derivative(ABC):
+
+
+def second_derivative(Mesh,coefficient,timestep):
+    '''
+    Calculates the second derivative in Fick's Law
+    '''
+    #The coefficient to be passed is a function
+    #Mesh is the mesh for which the derivative is to be evaluated.
     
-    @abstractmethod
-    def derivative_value(self,x):
-        pass
-     
-
-
-
-
+    second_derivative=np.empty([Mesh.n_nodes,1])
+    
+    #utilize a "phantom node" to be able to compute the second derivative at 
+    #The edges
+    for i in range(1,Mesh.n_nodes-1):
+        i_plus_1=Mesh.get_concentration_by_id(i+1,timestep)
+        i_minus_1=Mesh.get_concentration_by_id(i-1,timestep)
+        i_center=Mesh.get_concentration_by_id(i,timestep)
+        distance=Mesh.node_container[i+1].x-Mesh.node_container[i].x
+        second_derivative[i,0]=(i_plus_1-2*i_center+i_minus_1)/(distance**2)
+        second_derivative[i,0]=coefficient*second_derivative[i,0]
+    
+    return second_derivative
+    
+def first_derivative(Mesh,coefficient,timestep):  
+    '''
+    Calculates the first derivative in Fick's Law
+    '''
+    #The coefficient to be passed is a function
+    #Mesh is the mesh for which the derivative is to be evaluated.
+    
+    
+    first_derivative=np.empty([Mesh.n_nodes,1])
+        
+    #utilize a "phantom node" to be able to compute the second derivative at 
+    #The edges
+    for i in range(1,Mesh.n_nodes-1):
+        i_plus_1=Mesh.get_concentration_by_id(i+1,timestep)
+        i_minus_1=Mesh.get_concentration_by_id(i-1,timestep)
+        distance=Mesh.node_container[i+1].x-Mesh.node_container[i].x
+        first_derivative[i,0]=(i_plus_1-i_minus_1)/(distance)
+        first_derivative[i,0]=coefficient*first_derivative[i,0]
+        
+    return first_derivative
+        
+    
--- mesh.py
+++ mesh.py
@@ -1,12 +1,76 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 22 15:11:42 2021
+Created on Sat Dec 11 16:59:37 2021
 
 @author: antti
 """
 
 
-class Mesh():
+from node import Node_SPM as Node_SPM
+class Mesh1D():
+    '''
+    Mesh1D class
+    '''
+    #Need to make init empty, while creating mesh it is not really necessary
+    #To know the lengths etc. This is better done in some separate generator
+    #This way there is flexibility in how the mesh is created.
+    def __init__(self,n_timestep):
+        '''
+        Initialize Mesh1D class with
+        n_timestep
+        '''
+        self.node_id=0
+        #Initialize the container (list in this implementation holding the nodes)
+        self.node_container=[]
+        self.n_timestep=n_timestep
+        self.n_nodes=0
+
+    def add_node(self,x):
+        '''
+        Adds a node based on x location, returns new node
+        '''
+        self.node_container.append(Node(self,self.node_id,x))
+        self.node_id+=1
+        self.n_nodes+=1
+        return
+
+    def add_nodes(self,length,n_elements):
+        '''
+        Add nodes of a length with n_elements
+        '''
+        #helper variables
+        self.dr=length/n_elements
+        x=0.0
+        for i in range(n_elements+1):
+            self.add_node(x+i*self.dr)
+            
+class Mesh1D_SPM(Mesh1D):
+    '''
+    Mesh1D_SPM class, subset of Mesh1D
+    '''
+    def add_node(self,x,initial_concentration):
+        '''
+        Adds a node based on x location, returns new node
+        '''
+        self.node_container.append(Node_SPM(self,self.node_id,x,initial_concentration))
+        self.node_id+=1
+        self.n_nodes+=1
+        return
     
-    
+    def add_nodes(self,length,n_elements,initial_concentration):
+        '''
+        Add nodes of a length with n_elements
+        '''
+        #helper variables
+        self.dr=length/n_elements
+        x=0.0
+        for i in range(n_elements+1):
+            self.add_node(x+i*self.dr,initial_concentration)
+                
+    def get_concentration_by_id(self,node_id,timestep):
+        '''
+        Get concentration at a node, and return
+        '''
+        return self.node_container[node_id].concentration[0,timestep]
+        
