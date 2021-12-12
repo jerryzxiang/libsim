161c778de6c7a484f29a98be8c24211b4e1a4f32
Antti Valkonen <valkonen@princeton.edu>
2021-12-12 12:57:46 -0500

    wrote the first test to Mesh1D_SPM; this test passes

---
+++ tests/mesh__test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 21:13:37 2021
+
+@author: antti
+"""
+
+
+from libsim.mesh import Mesh1D_SPM as Mesh1D_SPM
+
+
+
+def test_spm_add_single_node():
+    
+    n_timestep=10
+    length=10.0
+    n_elements=10
+    dr=1.0
+    initial_concentration=7.0
+    x_0=5.0
+    mesh=Mesh1D_SPM(n_timestep)
+    mesh.add_node(x_0,initial_concentration)
+    
+    node=mesh.node_container[0]
+    assert mesh.n_nodes==1
+    assert mesh.node_id==1
+    assert len(mesh.node_container)==1
+    assert node.node_id==0
+    assert node.x==x_0
+    assert node.concentration[0,0]==initial_concentration
