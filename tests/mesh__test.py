7e2d736743136f58dcc796c2a3cc31402bfe70e7
Antti Valkonen <valkonen@princeton.edu>
2021-12-12 13:07:00 -0500

    added test to add_nodes; passes.

--- tests/mesh__test.py
+++ tests/mesh__test.py
@@ -29,3 +29,29 @@ def test_spm_add_single_node():
     assert node.node_id==0
     assert node.x==x_0
     assert node.concentration[0,0]==initial_concentration
+
+
+def test_spm_add_nodes():
+    
+    n_timestep=10
+    length=10.0
+    n_elements=10
+    dr=1.0
+    initial_concentration=7.0
+    x_0=0.0
+    mesh=Mesh1D_SPM(n_timestep)
+    mesh.add_nodes(length,n_elements,initial_concentration)
+    
+    node=mesh.node_container[0]
+    assert mesh.n_nodes==11
+    assert mesh.node_id==11
+    assert len(mesh.node_container)==11
+    
+    assert node.node_id==0
+    assert node.x==x_0
+    assert node.concentration[0,0]==initial_concentration
+    
+    node5=mesh.node_container[4]
+    assert node5.node_id==4
+    assert node5.x==4.0
+    assert node5.concentration[0,0]==initial_concentration
