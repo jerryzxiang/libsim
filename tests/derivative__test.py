0a33c5d4d69b34624b9df286face3cda4ab6ba91
Antti Valkonen <valkonen@princeton.edu>
2021-12-15 14:51:55 -0500

    amended tests for derivatives

--- tests/derivative__test.py
+++ tests/derivative__test.py
@@ -36,13 +36,33 @@ def test_first_derivative_initial(inp,expected):
     d1=first_derivative(mesh,1.0,timestep)
     comparison=np.zeros([mesh.n_nodes-2])
     assert d1[inp,0]==expected
+
+
+
+
+
+
+length=10.0
+n_elements=10
+dr=1.0
+initial_concentration=7.0
+x_0=0.0
+n_timestep=10
+mesh=Mesh1D_SPM(n_timestep)
+mesh.add_nodes(length,n_elements,initial_concentration)
+
+timestep=0
+
+#create the parameter list
+#no need to go through all of the indices, since first and last in the derivative
+#are not computed.
+test_parameter_list=[]
+for i in range(1,10):
+    test_parameter_list.append((i,0.0))
     
-mesh.node_container[2].concentration[0,0]=2
 
-
-
-
-def test_first_derivative_1():
-    d1=first_derivative(mesh,1.0,timestep)
+@pytest.mark.parametrize('inp, expected', test_parameter_list)
+def test_second_derivative_initial(inp,expected):
+    d2=second_derivative(mesh,1.0,timestep)
     comparison=np.zeros([mesh.n_nodes-2])
-    assert d1[1,0]==1.0
+    assert d2[inp,0]==expected
