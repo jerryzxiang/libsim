bug fix: wrong coefficients for the derivatives in the update formula caused the solution to blow up

libsim/derivative.py
@@ -10,7 +10,7 @@ def first_derivative(Mesh, coefficient, timestep):
     #The coefficient to be passed is a function
     #Mesh is the mesh for which the derivative is to be evaluated.
     
-    first_derivative = np.empty([Mesh.n_nodes,1])
+    first_derivative = np.zeros([Mesh.n_nodes,1])
         
     #utilize a "phantom node" to be able to compute the second derivative at 
     #The edges
@@ -19,7 +19,7 @@ def first_derivative(Mesh, coefficient, timestep):
         i_minus_1 = Mesh.get_concentration_by_id(i - 1, timestep)
         distance = 2.0 * (Mesh.node_container[i + 1].x - Mesh.node_container[i].x)
         first_derivative[i, 0] = (i_plus_1 - i_minus_1) / (distance)
-        first_derivative[i, 0] = coefficient * first_derivative[i, 0]
+ #       first_derivative[i, 0] = coefficient * first_derivative[i, 0]
         
     return first_derivative
         
@@ -29,7 +29,7 @@ def second_derivative(Mesh, coefficient, timestep):
     The coefficient to be passed is a function
     Mesh is the mesh for which the derivative is to be evaluated.
     '''
-    second_derivative = np.empty([Mesh.n_nodes,1])
+    second_derivative = np.zeros([Mesh.n_nodes,1])
     
     #utilize a "phantom node" to be able to compute the second derivative at 
     #The edges
@@ -38,6 +38,11 @@ def second_derivative(Mesh, coefficient, timestep):
         i_minus_1 = Mesh.get_concentration_by_id(i - 1, timestep)
         i_center = Mesh.get_concentration_by_id(i, timestep)
         distance = Mesh.node_container[i + 1].x - Mesh.node_container[i].x
-        second_derivative[i, 0] = (i_plus_1 - 2 * i_center + i_minus_1) / (distance ** 2)
+        try:
+            second_derivative[i, 0] = (i_plus_1 - 2 * i_center + i_minus_1) / (distance ** 2)
+        except:
+            print("error on timestep")
+            print(timestep)
+            raise
         second_derivative[i, 0] = coefficient*second_derivative[i, 0]    
     return second_derivative

libsim/main.py
@@ -10,6 +10,10 @@ from libsim.mesh import Mesh1D_SPM as Mesh1D_SPM
 from libsim.derivative import second_derivative
 from libsim.derivative import first_derivative
 
+
+
+np.seterr(all='raise')
+
 # constant
 FARADAY_NUMBER = 9.64853399e4
 
@@ -101,14 +105,15 @@ anode.mesh_initialize(R_ANODE, N_SEGMENTS, n_timestep, anode_initial_c)
 second_derivative(cathode.Mesh, 1.0, 1)
 
 A_cathode = cathode.calculate_effective_area()
-
-for i in range(1, n_timestep):
+endtime=n_timestep
+for i in range(0, endtime-1):
     cathode.simulation_step(i, DT)
-    anode.simulation_step(i, DT)
+
+
     
-voltage = battery_cell.get_voltage()
+#voltage = battery_cell.get_voltage()
 
 # plotting voltage vs time
 plt.figure()
-plt.plot(time_history, voltage)
+plt.plot(cathode.Mesh.node_container[10].concentration[0,:])
 plt.show()

libsim/node.py
@@ -28,6 +28,6 @@ class Node_SPM(Node):
         mesh, node_id, x, initial_concentration
         '''
         #The timehistory
-        self.concentration = np.empty((1, mesh.n_timestep))
+        self.concentration = np.zeros((1, mesh.n_timestep))
         self.concentration[0, 0] = initial_concentration
         super().__init__(mesh, node_id, x)