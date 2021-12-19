c56342834829ce04c2209b90c57f26b922509f78
Alexander Preston <36464459+alexandercpreston@users.noreply.github.com>
2021-12-19 14:26:56 -0500

    Update plot.py

--- libsim/plot.py
+++ libsim/plot.py
@@ -1,1 +1,9 @@
 # module to plot battery parameters over time
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+
+from batterycell import BatteryCell as BatteryCell
+from mesh import Mesh1D_SPM as Mesh1D_SPM
+import parameters.paramLibrary as pl
+import parameters.referencePotentials as rp
