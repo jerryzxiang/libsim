f04a38107748d5948bbeef8e02f1d90829344e1d
jzxiang <jzxiang@princeton.edu>
2021-12-19 13:46:59 -0500

    added referencePotentials for NMC LCO

--- libsim/parameters/paramLibrary.py
+++ libsim/parameters/paramLibrary.py
@@ -7,10 +7,10 @@ Similarly, there are 2 types of anodes:
 diffusivity,particle_radius,max_ion_concentration
 """
 cathodeDict = {
-    "LCO": [1.736E-14, 1.637E-7, 1.035E4],
     "LFP": [5.900E-18, 1.000E-8, 2.281E4],
+    "LCO": [1.736E-14, 1.637E-7, 1.035E4],
     "NMC": [1.000E-14, 5.300E-6, 4.823E4],
-    "NCA": [0000, 1.633E-6, 4.900E4],
+    "NCA": [3.000E-15, 1.633E-6, 4.900E4],
     }
 
 anodeDict = {
