0b111d14ec2802e99fc54934a4921d06ca970ba6
Alexander Preston <36464459+alexandercpreston@users.noreply.github.com>
2021-12-19 15:07:03 -0500

    plot functions

--- libsim/plot.py
+++ libsim/plot.py
@@ -1,15 +1,17 @@
 # module to plot battery parameters over time
-import numpy as np
-import argparse
 import matplotlib.pyplot as plt
 
 from batterycell import BatteryCell as BatteryCell
 from mesh import Mesh1D_SPM as Mesh1D_SPM
-import parameters.paramLibrary as pl
-import parameters.referencePotentials as rp
 
 # plotting voltage vs time
-def plot_voltage(cathode):
+def plot_voltage(voltage):
+  plt.figure()
+  plt.plot(voltage[0:9999])
+
+def plot_concentration(cathode):
   plt.figure()
   plt.plot(cathode.Mesh.node_container[10].concentration[0,:])
+
+def show_plots():
   plt.show()
