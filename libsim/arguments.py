ff7da9a2c5551cc75d4dbb4d5a073acf42857b64
Jose Yanez <jyanez@princeton.edu>
2021-12-20 12:02:46 -0800

    Sphinx Technicality

--- libsim/arguments.py
+++ libsim/arguments.py
@@ -20,76 +20,78 @@ A_INDEX = 0.18
 C_dict = pl.cathodeDict
 A_dict = pl.anodeDict
 
-# Argument parsing
-parser = argparse.ArgumentParser()
-parser.add_argument('cathode', type = str, 
-                    help = 'The first command line arg \
-                    is the cathode type:' 
-                    + str(list(C_dict.keys()))
-                    )
-parser.add_argument('anode', type = str,
-                    help = 
-                    'The second command line arg \
-                    is the anode type:'
-                    + str(list(A_dict.keys()))
-                    )
-parser.add_argument('input_current', type = float,
-                    help = 
-                    'The third command line arg \
-                    is the input current. Units: Amps.')
-parser.add_argument('capacity', type = float,
-                    help = 
-                    'The fourth command line arg \
-                    is the capacity. Units: Amp Hours.')
-parser.add_argument('internal_resistance', type = float,
-                    help = 
-                    'The fifth command line arg \
-                    is the internal resistance. Units: Ohms.')  
-parser.add_argument('N_segments', type = int,
-                    help = 
-                    'The sixth command line arg is the number \
-                    of radial segments N. The more radial segments, \
-                    the more accurate the simulation is. \"Default\" \
-                    value is 10.')
-parser.add_argument('simulation_time', type = int,
-                    help = 
-                    'The seventh command line arg is the simulation \
-                    time. Units: Seconds.'
-                    ) 
-parser.add_argument('dt', type = float,
-                    help = 
-                    'The eighth command line arg is the time step. \
-                    Units: Seconds.'
-                    ) 
-args = parser.parse_args()
+
+if __name__ == "arguments":
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cathode', type = str, 
+                        help = 'The first command line arg \
+                        is the cathode type:' 
+                        + str(list(C_dict.keys()))
+                        )
+    parser.add_argument('anode', type = str,
+                        help = 
+                        'The second command line arg \
+                        is the anode type:'
+                        + str(list(A_dict.keys()))
+                        )
+    parser.add_argument('input_current', type = float,
+                        help = 
+                        'The third command line arg \
+                        is the input current. Units: Amps.')
+    parser.add_argument('capacity', type = float,
+                        help = 
+                        'The fourth command line arg \
+                        is the capacity. Units: Amp Hours.')
+    parser.add_argument('internal_resistance', type = float,
+                        help = 
+                        'The fifth command line arg \
+                        is the internal resistance. Units: Ohms.')  
+    parser.add_argument('N_segments', type = int,
+                        help = 
+                        'The sixth command line arg is the number \
+                        of radial segments N. The more radial segments, \
+                        the more accurate the simulation is. \"Default\" \
+                        value is 10.')
+    parser.add_argument('simulation_time', type = int,
+                        help = 
+                        'The seventh command line arg is the simulation \
+                        time. Units: Seconds.'
+                        ) 
+    parser.add_argument('dt', type = float,
+                        help = 
+                        'The eighth command line arg is the time step. \
+                        Units: Seconds.'
+                        ) 
+    args = parser.parse_args()
 
 
-# assigning values based on command line args
-D_CATHODE = C_dict[args.cathode][0]
-R_CATHODE = C_dict[args.cathode][1]
-MAX_ION_CONCENTRATION_CATHODE = C_dict[args.cathode][2]
+    # assigning values based on command line args
+    D_CATHODE = C_dict[args.cathode][0]
+    R_CATHODE = C_dict[args.cathode][1]
+    MAX_ION_CONCENTRATION_CATHODE = C_dict[args.cathode][2]
 
-D_ANODE = A_dict[args.anode][0]
-R_ANODE = A_dict[args.anode][1]
-MAX_ION_CONCENTRATION_ANODE = A_dict[args.anode][2]
+    D_ANODE = A_dict[args.anode][0]
+    R_ANODE = A_dict[args.anode][1]
+    MAX_ION_CONCENTRATION_ANODE = A_dict[args.anode][2]
 
-INPUT_CURRENT = args.input_current
-CAPACITY_AMP_HR = args.capacity
-INTERNAL_RESISTANCE = args.internal_resistance
-N_SEGMENTS = args.N_segments
-SIMULATION_TIME = args.simulation_time
-DT = args.dt
+    INPUT_CURRENT = args.input_current
+    CAPACITY_AMP_HR = args.capacity
+    INTERNAL_RESISTANCE = args.internal_resistance
+    N_SEGMENTS = args.N_segments
+    SIMULATION_TIME = args.simulation_time
+    DT = args.dt
 
-# cathode segment length
-dR_CATHODE = R_CATHODE / N_SEGMENTS
-# anode segment length
-dR_ANODE = R_CATHODE / N_SEGMENTS
+    # cathode segment length
+    dR_CATHODE = R_CATHODE / N_SEGMENTS
+    # anode segment length
+    dR_ANODE = R_CATHODE / N_SEGMENTS
 
-# number of time steps rounded down to nearest integer
-n_timestep = math.ceil(SIMULATION_TIME / DT)
+    # number of time steps rounded down to nearest integer
+    n_timestep = math.ceil(SIMULATION_TIME / DT)
 
-# time history
-time_history = np.arange(0, SIMULATION_TIME, DT)
+    # time history
+    time_history = np.arange(0, SIMULATION_TIME, DT)
 
-# time increment in minutes
-minutes = np.linspace(0, SIMULATION_TIME / 60, len(time_history))
+    # time increment in minutes
+    minutes = np.linspace(0, SIMULATION_TIME / 60, len(time_history))
--- libsim/main.py
+++ libsim/main.py
@@ -1,5 +1,8 @@
 """
+Main
+
 Driver code file
+
 """
 import numpy as np
 import math
@@ -16,64 +19,69 @@ import parameters.referencePotentials as rp
 import arguments as ag
 import plot
 
-np.seterr(all = 'warn')
 
-def main():
-    # Instantiate battery_cell
-    battery_cell = BatteryCell(ag.CAPACITY_AMP_HR, ag.INTERNAL_RESISTANCE)
-    battery_cell.create_cathode(ag.D_CATHODE, ag.R_CATHODE, 
-                                ag.MAX_ION_CONCENTRATION_CATHODE)
-    battery_cell.create_anode(ag.D_ANODE, ag.R_ANODE, 
-                            ag.MAX_ION_CONCENTRATION_ANODE)
+if __name__ == "__main__":
+    np.seterr(all = 'warn')
 
-    # shorthand
-    cathode = battery_cell.cathode
-    anode = battery_cell.anode
-
-    # Create potential look up tables for cathode and anode
-    cathode_potential_ref = rp.cathode_potential_ref_array[ag.args.cathode]
-    anode_potential_ref = rp.anode_potential_ref_array[ag.args.anode]
-    cathode.create_potential_lookup_tables(cathode_potential_ref)
-    anode.create_potential_lookup_tables(anode_potential_ref)
     
-    # Initialize the cathode
-    cathode_initial_c = cathode.concentration_list[math.floor(ag.C_INDEX *
-                                    len(rp.cathode_potential_ref_array))]
-    cathode.mesh_initialize(ag.R_CATHODE, ag.N_SEGMENTS, 
-                            ag.n_timestep, cathode_initial_c)
 
-    # Initialize the anode
-    anode_initial_c = anode.concentration_list[math.floor(ag.A_INDEX *
-                                    len(rp.anode_potential_ref_array))]
-    anode.mesh_initialize(ag.R_ANODE, ag.N_SEGMENTS, 
-                        ag.n_timestep, anode_initial_c)
+    def main():
+        # Instantiate battery_cell
+        battery_cell = BatteryCell(ag.CAPACITY_AMP_HR, ag.INTERNAL_RESISTANCE)
+        battery_cell.create_cathode(ag.D_CATHODE, ag.R_CATHODE, 
+                                    ag.MAX_ION_CONCENTRATION_CATHODE)
+        battery_cell.create_anode(ag.D_ANODE, ag.R_ANODE, 
+                                ag.MAX_ION_CONCENTRATION_ANODE)
 
-    # Run simulation
-    for i in range(0, ag.n_timestep - 1):
-        cathode.simulation_step(i, ag.DT)
-        anode.simulation_step(i, ag.DT)
+        # shorthand
+        cathode = battery_cell.cathode
+        anode = battery_cell.anode
 
-    cathode_potential = cathode.electrode_potential(cathode.Mesh.node_container[ag.N_SEGMENTS])
-    anode_potential = anode.electrode_potential(anode.Mesh.node_container[ag.N_SEGMENTS])
-    voltage = battery_cell.get_voltage(cathode_potential, anode_potential, 
-                                    ag.INPUT_CURRENT, ag.INTERNAL_RESISTANCE)
-    
-    # Plot electrode potential vs time
-    plot.plot_voltage(cathode_potential,ag.minutes,
-                        'Reference Potential vs Time - Cathode - ' 
-                        + str(ag.args.cathode))
-    plot.plot_voltage(anode_potential,ag.minutes,
-                    'Reference Potential vs Time - Anode - '
-                    + str(ag.args.anode))                    
-    # Plot battery voltage vs time  
-    plot.plot_voltage(voltage, ag.minutes,
-                        'Battery Voltage vs. Time (Charging) - ' 
-                        + str(ag.args.cathode) + '/' 
-                        + str(ag.args.anode))
+        # Create potential look up tables for cathode and anode
+        cathode_potential_ref = rp.cathode_potential_ref_array[ag.args.cathode]
+        anode_potential_ref = rp.anode_potential_ref_array[ag.args.anode]
+        cathode.create_potential_lookup_tables(cathode_potential_ref)
+        anode.create_potential_lookup_tables(anode_potential_ref)
+        
+        # Initialize the cathode
+        cathode_initial_c = cathode.concentration_list[math.floor(ag.C_INDEX *
+                                        len(rp.cathode_potential_ref_array))]
+        cathode.mesh_initialize(ag.R_CATHODE, ag.N_SEGMENTS, 
+                                ag.n_timestep, cathode_initial_c)
 
-    # Plot concentrations for both electrodes
-    plot.plot_concentration(cathode, cathode_potential, 'Cathode')
-    plot.plot_concentration(anode, anode_potential, 'Anode')
+        # Initialize the anode
+        anode_initial_c = anode.concentration_list[math.floor(ag.A_INDEX *
+                                        len(rp.anode_potential_ref_array))]
+        anode.mesh_initialize(ag.R_ANODE, ag.N_SEGMENTS, 
+                            ag.n_timestep, anode_initial_c)
 
-# Run 
-main()
+        # Run simulation
+        for i in range(0, ag.n_timestep - 1):
+            cathode.simulation_step(i, ag.DT)
+            anode.simulation_step(i, ag.DT)
+
+        cathode_potential = cathode.electrode_potential(cathode.Mesh.node_container[ag.N_SEGMENTS])
+        anode_potential = anode.electrode_potential(anode.Mesh.node_container[ag.N_SEGMENTS])
+        voltage = battery_cell.get_voltage(cathode_potential, anode_potential, 
+                                        ag.INPUT_CURRENT, ag.INTERNAL_RESISTANCE)
+        
+        # Plot electrode potential vs time
+        plot.plot_voltage(cathode_potential,ag.minutes,
+                            'Reference Potential vs Time - Cathode - ' 
+                            + str(ag.args.cathode))
+        plot.plot_voltage(anode_potential,ag.minutes,
+                        'Reference Potential vs Time - Anode - '
+                        + str(ag.args.anode))                    
+        # Plot battery voltage vs time  
+        plot.plot_voltage(voltage, ag.minutes,
+                            'Battery Voltage vs. Time (Charging) - ' 
+                            + str(ag.args.cathode) + '/' 
+                            + str(ag.args.anode))
+
+        # Plot concentrations for both electrodes
+        plot.plot_concentration(cathode, cathode_potential, 'Cathode')
+        plot.plot_concentration(anode, anode_potential, 'Anode')
+
+
+    # Run 
+    main()
